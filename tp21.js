fc7ed645d7793ad219604997ba8e073f8c615a4a
lucas <lucasfiscella1@gmail.com>
2022-03-09 12:36:48 -0300

    TP Unidad 2

---
+++ tp21.js
@@ -0,0 +1,23 @@
+var cantidadDeVentanasEnMiCasa = 3;
+
+console.log(
+  "En mi casa hay un total de:" +
+    " " +
+    cantidadDeVentanasEnMiCasa +
+    " " +
+    "ventanas"
+);
+
+var soyHumano = true;
+
+console.log("Yo soy humano? " + soyHumano);
+
+var miGustoDePizzaPreferido = "Napolitana con Jamon";
+
+console.log("Mi gusto de pizza preferido es: " + miGustoDePizzaPreferido);
+
+var mismaVariable = "Hola";
+console.log(mismaVariable);
+
+var mismaVariable = "Minions";
+console.log(mismaVariable);
